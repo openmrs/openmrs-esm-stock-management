1f0636944a45c8bcba70819659041bdd25380900
jabahum <jabajnan8@gmail.com>
2023-08-23 23:44:37 +0300

    fix build errors

--- src/stock-items/components/filter-stock-items/filter-stock-items.test.tsx
+++
@@ -1,7 +0,0 @@
-jest.mock("@openmrs/esm-framework", () => {
-  const originalModule = jest.requireActual("@openmrs/esm-framework");
-  return {
-    ...originalModule,
-    openmrsFetch: jest.fn(),
-  };
-});
