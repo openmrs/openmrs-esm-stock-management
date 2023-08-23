911a50dfb56631d5a80dd03854b25c3fed6b9a0b
Isaac Obella <wizlif@users.noreply.github.com>
2023-08-23 23:22:10 +0300

    ft(core): overlay implementation
    Signed-off-by: Isaac Obella <wizlif@users.noreply.github.com>

--- src/dashbooard/stock-management-dashboard.component.tsx
+++ src/dashbooard/stock-management-dashboard.component.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { StockManagementHeader } from "../stock-management-header/stock-management-header.component";
 import StockCommodityTabs from "../stock-tabs/commodity-tabs.component";
+import Overlay from "../core/components/overlay/overlay.component";
 
 export default function StockManagementDashboard() {
   return (
     <div className={`omrs-main-content`}>
       <StockManagementHeader />
       <StockCommodityTabs />
+      <Overlay />
     </div>
   );
 }
