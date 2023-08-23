d72f94195c393c7c8c0814bb445cec51680fd133
Isaac Obella <wizlif@users.noreply.github.com>
2023-08-23 16:39:06 +0300

    ft(core): vertical tabs
    Signed-off-by: Isaac Obella <wizlif@users.noreply.github.com>

---
+++ src/core/components/tabs/vertical-tabs.component.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { useLayoutType } from "@openmrs/esm-framework";
+import { TabItem } from "./types";
+import { Tab, TabList, TabPanel, TabPanels, Tabs } from "@carbon/react";
+import styles from "./vertical-tabs.scss";
+
+interface VerticalTabsProps {
+  tabs: TabItem[];
+  title?: string;
+  hasContainer?: boolean;
+}
+
+const VerticalTabs: React.FC<VerticalTabsProps> = ({
+  tabs,
+  title,
+  hasContainer,
+}) => {
+  return (
+    <div
+      className={`
+        ${hasContainer ? styles.tabContainer : ""}
+        ${styles.cohortBuilder}
+      `}
+    >
+      {title && <p className={styles.heading}>{title}</p>}
+      <div className={styles.tab}>
+        <Tabs className={`${styles.verticalTabs}`}>
+          <TabList aria-label="navigation">
+            {tabs.map((tab: TabItem, index: number) => (
+              <Tab key={index} disabled={tab.disabled}>
+                {tab.name}
+              </Tab>
+            ))}
+          </TabList>
+          <TabPanels>
+            {tabs.map((tab: TabItem, index: number) => (
+              <TabPanel key={index}>{tab.component}</TabPanel>
+            ))}
+          </TabPanels>
+        </Tabs>
+      </div>
+    </div>
+  );
+};
+
+export default VerticalTabs;
