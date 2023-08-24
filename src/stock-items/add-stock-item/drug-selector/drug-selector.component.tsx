55906bfc791e7397eea1c3c19bd4ad560c59cada
Isaac Obella <wizlif@users.noreply.github.com>
2023-08-25 01:00:18 +0300

    ft(stock): upgrade reusable drugs selector to allow search
    Signed-off-by: Isaac Obella <wizlif@users.noreply.github.com>

--- src/stock-items/add-stock-item/drug-selector/drug-selector.component.tsx
+++ src/stock-items/add-stock-item/drug-selector/drug-selector.component.tsx
@@ -1,9 +1,8 @@
 import React, { ReactNode } from "react";
-import { ComboBox, TextInputSkeleton } from "@carbon/react";
-import { useDrugs } from "../../../stock-lookups/stock-lookups.resource";
-import { ResourceRepresentation } from "../../../core/api/api";
+import { ComboBox, InlineLoading } from "@carbon/react";
 import { Drug } from "../../../core/api/types/concept/Drug";
 import { Control, Controller, FieldValues } from "react-hook-form";
+import { useDrugsHook } from "./drug-selector.resource";
 
 interface DrugSelectorProps<T> {
   placeholder?: string;
@@ -20,44 +19,47 @@ interface DrugSelectorProps<T> {
 }
 
 const DrugSelector = <T,>(props: DrugSelectorProps<T>) => {
-  const {
-    items: { results: drugList },
-    isLoading,
-  } = useDrugs({
-    v: ResourceRepresentation.Default,
-  });
-
-  if (isLoading) return <TextInputSkeleton />;
+  const { isLoading, drugList, setSearchString } = useDrugsHook();
 
   return (
-    <Controller
-      name={props.controllerName}
-      control={props.control}
-      render={({ field: { onChange, value, ref } }) => (
-        <ComboBox
-          titleText={props.title}
-          name={props.name}
-          control={props.control}
-          controllerName={props.controllerName}
-          id={props.name}
-          size={"md"}
-          items={drugList || []}
-          onChange={(data: { selectedItem: Drug }) => {
-            props.onDrugChanged?.(data.selectedItem);
-            onChange(data.selectedItem.uuid);
-          }}
-          initialSelectedItem={
-            drugList?.find((p) => p.uuid === props.drugUuid) ?? ""
-          }
-          itemToString={drugName}
-          value={drugList?.find((p) => p.uuid === value)?.display ?? ""}
-          placeholder={props.placeholder}
-          invalid={props.invalid}
-          invalidText={props.invalidText}
-          ref={ref}
+    <div>
+      <Controller
+        name={props.controllerName}
+        control={props.control}
+        render={({ field: { onChange, value, ref } }) => (
+          <ComboBox
+            titleText={props.title}
+            name={props.name}
+            control={props.control}
+            controllerName={props.controllerName}
+            id={props.name}
+            size={"md"}
+            items={drugList || []}
+            onChange={(data: { selectedItem: Drug }) => {
+              props.onDrugChanged?.(data.selectedItem);
+              onChange(data.selectedItem.uuid);
+            }}
+            initialSelectedItem={
+              drugList?.find((p) => p.uuid === props.drugUuid) ?? ""
+            }
+            itemToString={drugName}
+            value={drugList?.find((p) => p.uuid === value)?.display ?? ""}
+            onInputChange={setSearchString}
+            placeholder={props.placeholder}
+            invalid={props.invalid}
+            invalidText={props.invalidText}
+            ref={ref}
+          />
+        )}
+      />
+      {isLoading && (
+        <InlineLoading
+          status="active"
+          iconDescription="Searching"
+          description="Searching..."
         />
       )}
-    />
+    </div>
   );
 };
 
