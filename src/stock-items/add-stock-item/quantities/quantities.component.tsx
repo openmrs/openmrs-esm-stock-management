204a43fb0d2ad8bac652bff1f6c8e82ddd1f312e
Isaac Obella <wizlif@users.noreply.github.com>
2023-08-25 01:06:50 +0300

    ft(stock): implement quantities tab
    Signed-off-by: Isaac Obella <wizlif@users.noreply.github.com>

--- src/stock-items/add-stock-item/quantities/quantities.component.tsx
+++ src/stock-items/add-stock-item/quantities/quantities.component.tsx
@@ -1,20 +1,66 @@
-import React from "react";
-import { useTranslation } from "react-i18next";
-import { useLocations } from "@openmrs/esm-framework";
-import addStockStyles from "../add-stock-item.scss";
+import React, { useEffect, useMemo } from "react";
+import { ResourceRepresentation } from "../../../core/api/api";
+import { formatDisplayDate } from "../../../core/utils/datetimeUtils";
+import { DataTableSkeleton, Tile } from "@carbon/react";
+import DataList from "../../../core/components/table/table.component";
+import styles from "../../stock-items-table.scss";
+import { useStockItemQuantitiesHook } from "./quantities.resource";
 
 interface StockQuantitiesProps {
   onSubmit?: () => void;
+  stockItemUuid: string;
 }
 
-const StockQuantities: React.FC<StockQuantitiesProps> = () => {
-  const { t } = useTranslation();
+const StockQuantities: React.FC<StockQuantitiesProps> = ({ stockItemUuid }) => {
+  const {
+    isLoading,
+    items,
+    tableHeaders,
 
-  const locations = useLocations();
+    totalCount,
+    setCurrentPage,
+    setStockItemUuid,
+  } = useStockItemQuantitiesHook(ResourceRepresentation.Default);
+
+  useEffect(() => {
+    setStockItemUuid(stockItemUuid);
+  }, [stockItemUuid, setStockItemUuid]);
+
+  const tableRows = useMemo(() => {
+    return items?.map((row, index) => ({
+      ...row,
+      id: `${row.partyUuid}${row.stockBatchUuid}${index}`,
+      key: `${row.partyUuid}${row.stockBatchUuid}${index}`,
+      uuid: `${row.partyUuid}${row.stockBatchUuid}${index}`,
+      expires: formatDisplayDate(row?.expiration),
+      location: row?.partyName,
+      quantity: row?.quantity?.toLocaleString() ?? "",
+      batch: row.batchNumber ?? "",
+      packaging: row.quantityUoM ?? "",
+    }));
+  }, [items]);
+
+  if (isLoading) {
+    return <DataTableSkeleton role="progressbar" />;
+  }
+
+  if (items?.length != undefined) {
+    return (
+      <DataList
+        columns={tableHeaders}
+        data={tableRows}
+        totalItems={totalCount}
+        goToPage={setCurrentPage}
+        hasToolbar={false}
+      />
+    );
+  }
 
   return (
-    <div className={addStockStyles.formContainer}>
-      Quantities Coming soon ...
+    <div className={styles.tileContainer}>
+      <Tile className={styles.tile}>
+        <p className={styles.content}>No transactions to display</p>
+      </Tile>
     </div>
   );
 };
