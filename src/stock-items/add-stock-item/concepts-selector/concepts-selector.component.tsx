e386be459ba345766fcd5c510042532ca49a91a6
Isaac Obella <wizlif@users.noreply.github.com>
2023-08-25 00:58:28 +0300

    ft(stock): implement reusable concepts selection component
    Signed-off-by: Isaac Obella <wizlif@users.noreply.github.com>

---
+++ src/stock-items/add-stock-item/concepts-selector/concepts-selector.component.tsx
@@ -0,0 +1,64 @@
+import React, { ReactNode } from "react";
+import { Concept } from "../../../core/api/types/concept/Concept";
+import { Control, Controller, FieldValues } from "react-hook-form";
+import { useConcepts } from "../../../stock-lookups/stock-lookups.resource";
+import { ComboBox, TextInputSkeleton } from "@carbon/react";
+
+interface ConceptsSelectorProps<T> {
+  conceptUuid?: string;
+  onConceptUuidChange?: (unit: Concept) => void;
+  title?: string;
+  placeholder?: string;
+  invalid?: boolean;
+  invalidText?: ReactNode;
+
+  // Control
+  controllerName: string;
+  name: string;
+  control: Control<FieldValues, T>;
+}
+
+const ConceptsSelector = <T,>(props: ConceptsSelectorProps<T>) => {
+  const {
+    items: { results: concepts },
+    isLoading,
+  } = useConcepts({});
+
+  if (isLoading) return <TextInputSkeleton />;
+
+  return (
+    <Controller
+      name={props.controllerName}
+      control={props.control}
+      render={({ field: { onChange, value, ref } }) => (
+        <ComboBox
+          titleText={props.title}
+          name={props.name}
+          control={props.control}
+          controllerName={props.controllerName}
+          id={props.name}
+          size={"md"}
+          items={concepts || []}
+          onChange={(data: { selectedItem: Concept }) => {
+            props.onConceptUuidChange?.(data.selectedItem);
+            onChange(data.selectedItem.uuid);
+          }}
+          initialSelectedItem={
+            concepts?.find((p) => p.uuid === props.conceptUuid) || {}
+          }
+          itemToString={(item?: Concept) =>
+            item && item?.display ? `${item?.display}` : ""
+          }
+          shouldFilterItem={() => true}
+          value={concepts?.find((p) => p.uuid === value)?.display ?? ""}
+          placeholder={props.placeholder}
+          ref={ref}
+          invalid={props.invalid}
+          invalidText={props.invalidText}
+        />
+      )}
+    />
+  );
+};
+
+export default ConceptsSelector;
---
+++ src/stock-items/add-stock-item/concepts-selector/concepts-selector.test.tsx
@@ -0,0 +1,8 @@
+import { cleanup } from "@testing-library/react";
+
+describe("Test the concepts selector", () => {
+  afterEach(cleanup);
+  it(`renders without dying`, () => {
+    // render(<ConceptsSelector />);
+  });
+});
