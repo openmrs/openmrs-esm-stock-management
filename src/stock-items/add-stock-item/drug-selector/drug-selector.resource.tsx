b691d1227ffe110570c8b7598f3155d44c29a37b
Isaac Obella <wizlif@users.noreply.github.com>
2023-08-25 01:00:18 +0300

    ft(stock): upgrade reusable drugs selector to allow search

---
+++ src/stock-items/add-stock-item/drug-selector/drug-selector.resource.tsx
@@ -0,0 +1,51 @@
+import { ResourceRepresentation } from "../../../core/api/api";
+import { useEffect, useState } from "react";
+import {
+  ConceptFilterCriteria,
+  useDrugs,
+} from "../../../stock-lookups/stock-lookups.resource";
+import { debounce } from "../../../core/utils/debounce";
+
+export function useDrugsHook(filter?: ConceptFilterCriteria) {
+  const [conceptFilter, setConceptFilter] = useState<ConceptFilterCriteria>(
+    filter || {
+      v: ResourceRepresentation.Default,
+      limit: 10,
+      startIndex: 0,
+    }
+  );
+
+  const {
+    items: { results: drugList },
+    isLoading,
+  } = useDrugs(conceptFilter);
+
+  const [searchString, setSearchString] = useState(null);
+
+  // Drug filter type
+  const [limit, setLimit] = useState(filter?.limit || 10);
+  const [representation, setRepresentation] = useState(
+    filter?.v || ResourceRepresentation.Default
+  );
+
+  useEffect(() => {
+    debounce(
+      () =>
+        setConceptFilter({
+          startIndex: 0,
+          v: representation,
+          limit: limit,
+          q: searchString,
+        }),
+      500
+    );
+  }, [searchString, limit, representation]);
+
+  return {
+    drugList,
+    setLimit,
+    setRepresentation,
+    setSearchString,
+    isLoading,
+  };
+}
