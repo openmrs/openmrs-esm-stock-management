44c552acae3e9fc6d727921ae558b2b057f594bd
jecihjoy <jecihjoy@gmail.com>
2023-12-12 12:24:36 +0300

    Fix lint issue

--- src/stock-items/stock-items-table.resource.ts
+++ src/stock-items/stock-items-table.resource.ts
@@ -78,7 +78,7 @@ export function useStockItemsPages(v?: ResourceRepresentation) {
       {
         id: 7,
         key: "actions",
-        header: "Actions"
+        header: "Actions",
       },
     ],
     [t]
