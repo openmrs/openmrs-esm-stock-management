6f4c6fee9a8c73ad05490d6a5523a77e89f3f1e0
Donald Kibet <chelashawdonald@yahoo.com>
2024-01-08 15:48:14 +0300

    (wip) stock operations

--- src/stock-operations/stock-operations-table.component.tsx
+++ src/stock-operations/stock-operations-table.component.tsx
@@ -87,6 +87,8 @@ const StockOperations: React.FC<StockOperationsTableProps> = () => {
     totalCount: true,
   });
 
+  console.log(items);
+
   let operations: StockOperationType[] | null | undefined;
 
   const tableRows = useMemo(() => {
