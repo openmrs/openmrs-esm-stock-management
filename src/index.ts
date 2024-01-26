8d13653b3eb4fce96bb0b7f230aaf45aea192b8c
Jabar Jeremy <24471994+jabahum@users.noreply.github.com>
2024-01-26 11:39:10 +0300

    add stock management menu item (#81)
    * add stock management menu item
    
    * add slot
    
    * add meta attribute

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
-import { getAsyncLifecycle, defineConfigSchema } from "@openmrs/esm-framework";
+import { defineConfigSchema, getSyncLifecycle } from "@openmrs/esm-framework";
 import { configSchema } from "./config-schema";
+import appMenu from "./stock-app-menu-item/item.component";
+import bulkImportComponent from "./stock-items/add-bulk-stock-item/stock-items-bulk-import.component";
+import deleteUserModalComponent from "./stock-user-role-scopes/delete-stock-user-scope-modal.component";
+import deleteStockModalComponent from "./stock-sources/delete-stock-modal.component";
+import stockOperationDialogComponent from "./stock-operations/stock-operations-dialog/stock-operations-dialog.component";
+import stockManagementDashboardComponent from "./dashboard/stock-management-dashboard.component";
+import stockManagementComponent from "./stock-management.component";
+import stockManagementAdminCardLinkComponent from "./stock-management-admin-card-link.component";
 
 const moduleName = "@ugandaemr/esm-stock-management-app";
 
@@ -15,63 +23,41 @@ export const importTranslation = require.context(
   "lazy"
 );
 
-export const stockManagementAdminCardLink = getAsyncLifecycle(
-  () => import("./stock-management-admin-card-link.component"),
+export const stockManagementAdminCardLink = getSyncLifecycle(
+  stockManagementAdminCardLinkComponent,
   options
 );
 
-export const stockManagement = getAsyncLifecycle(
-  () => import("./stock-management.component"),
+export const stockManagement = getSyncLifecycle(
+  stockManagementComponent,
   options
 );
 
-export const stockManagementDashboard = getAsyncLifecycle(
-  () => import("./dashboard/stock-management-dashboard.component"),
+export const stockManagementDashboard = getSyncLifecycle(
+  stockManagementDashboardComponent,
   options
 );
 
-export const stockOperationDialog = getAsyncLifecycle(
-  () =>
-    import(
-      "./stock-operations/stock-operations-dialog/stock-operations-dialog.component"
-    ),
+export const stockOperationDialog = getSyncLifecycle(
+  stockOperationDialogComponent,
   options
 );
-export const deleteStockModal = getAsyncLifecycle(
-  () => import("./stock-sources/delete-stock-modal.component"),
-  {
-    featureName: "delete-stock-modal",
-    moduleName,
-  }
-);
+export const deleteStockModal = getSyncLifecycle(deleteStockModalComponent, {
+  featureName: "delete-stock-modal",
+  moduleName,
+});
 
-export const deleteUserScopeModal = getAsyncLifecycle(
-  () =>
-    import("./stock-user-role-scopes/delete-stock-user-scope-modal.component"),
-  {
-    featureName: "delete-stock-user-scope-modal",
-    moduleName,
-  }
-);
+export const deleteUserScopeModal = getSyncLifecycle(deleteUserModalComponent, {
+  featureName: "delete-stock-user-scope-modal",
+  moduleName,
+});
 
-export const deleteStockRuleModal = getAsyncLifecycle(
-  () =>
-    import(
-      "./stock-items/add-stock-item/stock-item-rules/delete-stock-rule-modal.component"
-    ),
-  {
-    featureName: "delete-stock-rule-modal",
-    moduleName,
-  }
-);
-
-export const importBulkStockItemsDialog = getAsyncLifecycle(
-  () =>
-    import(
-      "./stock-items/add-bulk-stock-item/stock-items-bulk-import.component"
-    ),
+export const importBulkStockItemsDialog = getSyncLifecycle(
+  bulkImportComponent,
   options
 );
+
+export const stockManagementAppMenuItem = getSyncLifecycle(appMenu, options);
 
 export function startupApp() {
   defineConfigSchema(moduleName, configSchema);
