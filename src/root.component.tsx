972dc5e66afe35713da119b982ebcfc86f7381bc
Dennis Kigen <kigen.work@gmail.com>
2024-03-01 11:25:13 +0300

    (fix) Remove global styles

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,10 +1,5 @@
 import React from "react";
 
-const swrConfiguration = {
-  // Maximum number of retries when the backend returns an error
-  errorRetryCount: 3,
-};
-
 const Root: React.FC = () => {
   return <></>;
 };
