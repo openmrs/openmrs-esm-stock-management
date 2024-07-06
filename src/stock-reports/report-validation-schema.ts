0450fc9b8d8cccd664b4606735c72c8f9b12bce0
Makombe Kennedy <kennedymakombe@gmail.com>
2024-07-06 11:35:48 +0300

    (fix):Fix report validation for required fields (#152)

--- src/stock-reports/report-validation-schema.ts
+++ src/stock-reports/report-validation-schema.ts
@@ -1,40 +1,77 @@
 import { z } from "zod";
 
-export const reportSchema = z.object({
-  startDate: z.coerce.date().refine((date) => date !== undefined, {
-    message: "Start Date is required",
-  }),
-  endDate: z.coerce.date().refine((date) => date !== undefined, {
-    message: "End Date is required",
-  }),
-  location: z.string({ required_error: "Location Required" }).min(1, {
-    message: "Location Required",
-  }),
-  reportName: z.string({ required_error: "Report Name Required" }).min(1, {
-    message: "Report Name Required",
-  }),
-  stockReportItemCategory: z.string().optional(),
-  mostLeastMoving: z.string().optional(),
-  mostLeastMovingName: z.string().optional(),
-  stockItemUuid: z.string().optional(),
-  stockItemName: z.string().optional(),
-  patientUuid: z.string().optional(),
-  patientName: z.string().optional(),
-  locationUuid: z.string().optional(),
-  childLocations: z.boolean().optional(),
-  stockSourceUuid: z.string().optional(),
-  stockSource: z.string().optional(),
-  stockSourceDestinationUuid: z.string().optional(),
-  stockSourceDestination: z.string().optional(),
-  inventoryGroupBy: z.string().optional(),
-  inventoryGroupByName: z.string().optional(),
-  stockItemCategoryConceptUuid: z.string().optional(),
-  reportSystemName: z.string().optional(),
-  stockItemCategory: z.string().optional(),
-  maxReorderLevelRatio: z.number().optional(),
-  fullFillment: z.string().array().optional(),
-  limit: z.string().optional(),
-  date: z.date().optional(),
-});
+export const reportSchema = z
+  .object({
+    startDate: z.coerce.date().optional(),
+    endDate: z.coerce.date().optional(),
+    location: z.string({ required_error: "Location Required" }).min(1, {
+      message: "Location Required",
+    }),
+    reportName: z.string({ required_error: "Report Name Required" }).min(1, {
+      message: "Report Name Required",
+    }),
+    stockReportItemCategory: z.string().optional(),
+    mostLeastMoving: z.string().optional(),
+    mostLeastMovingName: z.string().optional(),
+    stockItemUuid: z.string().optional(),
+    stockItemName: z.string().optional(),
+    patientUuid: z.string().optional(),
+    patientName: z.string().optional(),
+    locationUuid: z.string().optional(),
+    childLocations: z.boolean().optional(),
+    stockSourceUuid: z.string().optional(),
+    stockSource: z.string().optional(),
+    stockSourceDestinationUuid: z.string().optional(),
+    stockSourceDestination: z.string().optional(),
+    inventoryGroupBy: z.string().optional(),
+    stockItemCategory: z.string().optional(),
+    inventoryGroupByName: z.string().optional(),
+    stockItemCategoryConceptUuid: z.string().optional(),
+    reportSystemName: z.string().optional(),
+    maxReorderLevelRatio: z.number().optional(),
+    fullFillment: z.string().array().optional(),
+    limit: z.string().optional(),
+    date: z.coerce.date().optional(),
+  })
+  .superRefine((data, ctx) => {
+    if (
+      data.reportName !== "Stock Status Report" &&
+      data.reportName !== "Stock-Out Report"
+    ) {
+      if (!data.startDate) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: "Start Date is required ",
+          path: ["startDate"],
+        });
+      }
+      if (!data.endDate) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: "End Date is required",
+          path: ["endDate"],
+        });
+      }
+    }
+    if (
+      data.reportName === "Stock Status Report" ||
+      data.reportName === "Stock-Out Report"
+    ) {
+      if (!data.date) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: "Date is required ",
+          path: ["date"],
+        });
+      }
+      if (!data.inventoryGroupBy) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: "Inventory by is required ",
+          path: ["inventoryGroupBy"],
+        });
+      }
+    }
+  });
 
 export type StockReportSchema = z.infer<typeof reportSchema>;
