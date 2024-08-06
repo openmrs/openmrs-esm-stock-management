d15bf9bd0b1b1289ba26fdd56db8cd5e808f102d
felix <fkiprotich845@gmail.com>
2024-08-06 18:13:49 +0300

    effected selecteion of initial dispensingpackageunit and defaultstockoperations package unit when added or edited

--- src/stock-items/add-stock-item/stock-item-units-edit/stock-item-units-edit.component.tsx
+++ src/stock-items/add-stock-item/stock-item-units-edit/stock-item-units-edit.component.tsx
@@ -21,7 +21,7 @@ const StockItemUnitsEdit: React.FC<StockItemUnitsEditProps> = ({
   const { t } = useTranslation();
 
   const { item: stockItem, isLoading } = useStockItem(stockItemUuid);
-
+  console.log(stockItem)
   if (
     !(
       stockItem &&
@@ -34,7 +34,7 @@ const StockItemUnitsEdit: React.FC<StockItemUnitsEditProps> = ({
   return (
     <>
       <DispensingPackageMeasurement
-        dispensingUnitPackagingUoMUuid={stockItem?.purchasePriceUoMUuid}
+        dispensingUnitPackagingUoMUuid={stockItem?.dispensingUnitPackagingUoMUuid}
         name="dispensingUnitPackagingUoMUuid"
         controllerName="dispensingUnitPackagingUoMUuid"
         control={control}
@@ -52,7 +52,7 @@ const StockItemUnitsEdit: React.FC<StockItemUnitsEditProps> = ({
         }
       />
       <DispensingPackageMeasurement
-        dispensingUnitPackagingUoMUuid={stockItem?.purchasePriceUoMUuid}
+        dispensingUnitPackagingUoMUuid={stockItem?.defaultStockOperationsUoMUuid}
         name="defaultStockOperationsUoMUuid"
         controllerName="defaultStockOperationsUoMUuid"
         control={control}
