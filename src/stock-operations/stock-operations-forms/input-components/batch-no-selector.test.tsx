46a46b2683a48f988c0cb5581c9359da00e091f0
Laurent Ouma <lawiomosh3@gmail.com>
2025-06-12 08:47:56 +0300

    fix batch-no selector broken test

--- src/stock-operations/stock-operations-forms/input-components/batch-no-selector.test.tsx
+++ src/stock-operations/stock-operations-forms/input-components/batch-no-selector.test.tsx
@@ -52,8 +52,8 @@ describe('BatchNoSelector', () => {
 
     mockUseStockItemBatchInformationHook.mockReturnValue({
       items: [
-        { batchNumber: 'BATCH-001', quantity: 10 },
-        { batchNumber: 'BATCH-002', quantity: 20 },
+        { batchNumber: 'BATCH-001', quantity: 10, quantityUoM: 'Tablet' },
+        { batchNumber: 'BATCH-002', quantity: 20, quantityUoM: 'Tablet' },
       ] as StockItemInventory[],
       totalCount: 2,
       currentPage: 1,
@@ -118,7 +118,7 @@ describe('BatchNoSelector', () => {
     await user.click(combobox);
     await user.type(combobox, 'BATCH-001');
 
-    const option = screen.getByText(`BATCH-001 | Qty: 10 | Expiry: ${formatForDatePicker(mockExpiration)}`);
+    const option = screen.getByText(`BATCH-001 | Qty: 10 Tablet | Expiry: ${formatForDatePicker(mockExpiration)}`);
 
     await user.click(option);
 
