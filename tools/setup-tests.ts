c128040cf9bcce3e6a063dca5236ed8a6a171881
Dennis Kigen <kigen.work@gmail.com>
2025-10-03 20:59:23 +0300

    (test) Add ResizeObserver mock and update Carbon to align with framework (#365)
    Adds a `ResizeObserver` global mock to fix Jest test failures when testing components that use Carbon Design System components (e.g., TextArea, DatePicker, DataTable) which require ResizeObserver in the browser environment.
    
    Also updates `@carbon/react` from `^1.83.0` to `^1.92.1` to align with the version used in
    `@openmrs/esm-framework`.

--- tools/setup-tests.ts
+++ tools/setup-tests.ts
@@ -3,3 +3,10 @@ import '@testing-library/jest-dom';
 // https://github.com/jsdom/jsdom/issues/1695
 // eslint-disable-next-line @typescript-eslint/no-empty-function
 window.HTMLElement.prototype.scrollIntoView = function () {};
+
+// Mock ResizeObserver for Carbon components
+global.ResizeObserver = jest.fn().mockImplementation(() => ({
+  observe: jest.fn(),
+  unobserve: jest.fn(),
+  disconnect: jest.fn(),
+}));
